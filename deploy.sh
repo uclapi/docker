ab70f6c11a9d8e55e408a5527e6a463af8386bb6
Christopher Hammond <christopherhammond13@gmail.com>
2019-01-31 15:56:32 +0000

    Adds deployment script to ensure that env vars get passed to docker stack deploy

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "UCL API Magic Deployment Script"
+
+if [ -z $1 ]
+then
+    echo "You must pass either prod or staging as an environment variable"
+    exit 1
+fi
+
+# Combine the base Compose file with an environment-specific Compose file and build
+docker-compose -f docker-compose.yml -f docker-compose.$1.yml build
+
+# Then push to the Docker repository listed in the Compose file
+docker-compose -f docker-compose.yml -f docker-compose.$1.yml push
+
+# Dump the full configuration out to a temporary compose file
+# This is necessary because of https://github.com/moby/moby/issues/29133
+docker-compose -f docker-compose.yml -f docker-compose.$1.yml config > docker-compose-tmp.yml
+
+# Deploy the temporary Compose file
+docker stack deploy -c docker-compose-tmp.yml --with-registry-auth uclapi
+
+# Delete the temporary Compose file ready for the next run
+rm docker-compose-tmp.yml
+
+echo "Hopefully, deployment was successful!
